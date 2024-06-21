e875421ac8ef050859e82713cd1644c2f2cd822c
DESKTOP-2TMEOMQ\user <gunhok99@gmail.com>
2024-06-21 18:52:13 +0900

    6/21자 문제

---
+++ src/main/java/com/example/lesson05/Lesson05Controller.java
@@ -0,0 +1,75 @@
+package com.example.lesson05;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/lesson05")
+@Controller
+public class Lesson05Controller {
+
+	@GetMapping("/quiz01")
+	public String quiz01() {
+		return "lesson05/quiz01";
+	}
+	
+	@GetMapping("/quiz02")
+	public String quiz02(Model model) {
+		List<String> musicRanking = new ArrayList<>();
+		musicRanking.add("강남스타일");
+		musicRanking.add("벚꽃엔딩");
+		musicRanking.add("좋은날");
+		musicRanking.add("거짓말");
+		musicRanking.add("보고싶다");
+		
+		model.addAttribute("musicRanking", musicRanking);
+		
+		
+		List<Map<String, Object>> membership = new ArrayList<>();
+		
+		Map<String, Object> member = new HashMap<>();
+		member.put("name", "손오공");
+		member.put("phoneNumber", "010-1234-5678");
+		member.put("grade", "VIP");
+		member.put("point", 12040);
+		membership.add(member);
+
+		member = new HashMap<>();
+		member.put("name", "저팔계");
+		member.put("phoneNumber", "010-4321-1234");
+		member.put("grade", "BASIC");
+		member.put("point", 3420);
+		membership.add(member);
+
+		member = new HashMap<>();
+		member.put("name", "사오정");
+		member.put("phoneNumber", "010-8888-1111");
+		member.put("grade", "GOLD");
+		member.put("point", 1530);
+		membership.add(member);
+
+		member = new HashMap<>();
+		member.put("name", "삼장");
+		member.put("phoneNumber", "010-0000-1234");
+		member.put("grade", "GOLD");
+		member.put("point", 8450);
+		membership.add(member);
+
+		member = new HashMap<>();
+		member.put("name", "우마왕");
+		member.put("phoneNumber", "010-1111-2222");
+		member.put("grade", "BASIC");
+		member.put("point", 420);
+		membership.add(member);
+		
+		model.addAttribute("membership", membership);
+		return "lesson05/quiz02";
+	}
+	
+}
